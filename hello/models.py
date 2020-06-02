576fea29cf70fd8340e062c2c560069d6b347ea9
Lucas Silva <knust.green@gmail.com>
2020-06-01 21:37:38 -0300

    Demo3

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    resumo = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
