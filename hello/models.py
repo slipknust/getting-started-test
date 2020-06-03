c401a5cd1369b8ad646e27e4ea4280ef1ffbd4cb
Lucas Silva <knust.green@gmail.com>
2020-06-03 15:41:59 -0300

    DemoLucasTeste11

--- hello/models.py
+++ hello/models.py
@@ -14,3 +14,20 @@ class Post(models.Model):
     content = RichTextUploadingField(null=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     created_at = models.DateField(auto_now_add=True)
+
+class Hello(models.Model):
+
+    STATUS = (
+        (1, 'Doing'),
+        (2, 'Done'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.CharField(
+        max_length=1,
+        choices=STATUS,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
